301218bb6e4575537123c697d2ef1b563bbce7fa
lohith <lohith211@gmail.com>
2020-04-20 13:04:15 +0530

    shoppingcart changes2

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,14 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Subject, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
-	// Local variable which stores
-	public cartItems = [];
-	public products = new Subject();
-
 	constructor(private http: HttpClient) {}
 	getProducts() {
 		//console.log(this.API_URL + '?page=' + pageno);
